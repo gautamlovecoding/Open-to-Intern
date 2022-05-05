bb4b66bdb8e02474b7864880e4bdec22030b3036
gautam kumar <gautam01kr11@gmail.com>
2022-05-05 18:00:15 +0530

    Done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,54 +1,55 @@
 const college = require("../models/collegeModel");
 const intern = require("../models/internModel");
 
-const createCollege = async function(req, res){
+const createCollege = async function (req, res) {
     /*****************************Validation***********************************************/
-    try{ 
+    try {
         let data = req.body;
-    if(!Object.keys(data).length)
-    return res.status(400).send({status: false, message: "You must enter data"})
+        if (!Object.keys(data).length)
+            return res.status(400).send({ status: false, message: "You must enter data" })
 
-    if(!data.name) return res.status(400).send({status: false, message: "You must enter name"})
-    if(!data.fullName) return res.status(400).send({status: false, message: "You must enter full name"})
-    if(!data.logoLink) return res.status(400).send({status: false, message: "You must enter logoLink"})
+        if (!data.name) return res.status(400).send({ status: false, message: "You must enter name" })
+        if (!data.fullName) return res.status(400).send({ status: false, message: "You must enter full name" })
+        if (!data.logoLink) return res.status(400).send({ status: false, message: "You must enter logoLink" })
 
-    if(!data.name.trim().match(/^[a-zA-Z]+$/)) // REGEX using .match()
-     return res.status(400).send({status: false, msg: "Enter a valid name."})
+        if (!data.name.trim().match(/^[a-zA-Z]+$/)) // REGEX using .match()
+            return res.status(400).send({ status: false, msg: "Enter a valid name." })
 
-     if(!data.fullName.trim().match(/^[a-zA-Z,\-.\s]*$/)) // REGEX using .match()
-     return res.status(400).send({status: false, msg: "Enter a valid full name."})
-     
-     /*************************************************************************************/
 
-    let created = await college.create(data)
-    res.status(201).send({status: true, data: created})
+        if (!data.fullName.trim().match(/^[a-zA-Z,\-.\s]*$/)) // REGEX using .match()
+            return res.status(400).send({ status: false, msg: "Enter a valid full name." })
+
+        /*************************************************************************************/
+
+        let created = await college.create(data)
+        res.status(201).send({ status: true, data: created })
+    }
+    catch (err) {
+        console.log(err.message)
+        res.status(500).send({ status: false, msg: err.message })
+    }
+
 }
-catch(err){
-    console.log(err.message)
-    res.status(500).send({status: false, msg: err.message})
-}
-
-}
 
 
-const getCollege = async function (req,res){
+const getCollege = async function (req, res) {
     let getData = req.query
     /*****************************Validation*****************************************************************/
-    if(!getData.collegeName) return res.status(400).send({status: false, message: "You must enter your filter"})
-    let findCollege = await college.findOne({name: getData.collegeName})
+    if (!getData.collegeName) return res.status(400).send({ status: false, message: "You must enter your filter" })
+    let findCollege = await college.findOne({ name: getData.collegeName })
     // console.log(findCollege);
-    if(!findCollege) return res.status(404).send({status: false, message: "Document not found"})
+    if (!findCollege) return res.status(404).send({ status: false, message: "Document not found" })
     /*******************************************************************************************************/
     let collegeId = findCollege._id
-    let findIntern = await intern.find({collegeId: collegeId, isDeleted: false}).select({_id:1, name:1, email:1, mobile:1})
+    let findIntern = await intern.find({ collegeId: collegeId, isDeleted: false }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
 
     return res.status(200).send({
         status: true,
-        data:{
+        data: {
             "name": findCollege.name,
             "fullName": findCollege.fullName,
             "logoLink": findCollege.logoLink,
-            "interests":findIntern
+            "interests": findIntern
         }
     })
 }
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -9,16 +9,23 @@ const createIntern = async function (req, res) {
         if (!Object.keys(data).length)
             return res.status(400).send({ status: false, message: "You must enter data" })
 
-        if (!data.collegeName) return res.status(400).send({ status: false, message: "You must Give college name" })
+        if (!data.collegeName) return res.status(400).send({ status: false, message: "You must give college name" })
         if (!data.name) return res.status(400).send({ status: false, message: "You must enter name" })
         if (!data.name.trim().match(/^[a-zA-Z,\s]*$/)) // REGEX using .match()
             return res.status(400).send({ status: false, msg: "Enter a valid name." })
 
-        if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email.trim())))
+        if(!(/^[A-Za-z0-9_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/.test(data.email.trim())))
         return res.status(400).send({status: false, msg: "Enter a valid email address."})
 
+        let isRegisteredEmail = await intern.find({ email: data.email });
+        if (isRegisteredEmail.length) return res.status(400).send({ status: false, message: "email id already registered" });
+         
         if(!data.mobile.trim().match(/^(\+\d{1,3}[- ]?)?\d{10}$/))
-        return res.status(400).send({status: false, msg: "Enter a valid mobile number."})
+        return res.status(400).send({status: false, msg: "Enter a valid mobile number"})
+
+        let isRegisteredMobile = await intern.find({ mobile: data.mobile });
+        if (isRegisteredMobile.length) return res.status(400).send({ status: false, msg: "mobile number already registered" });
+    
 
         let findCollege = await college.findOne({ name: data.collegeName })
 
